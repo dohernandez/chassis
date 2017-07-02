25dd42db6f879ab55d0cbcdb02e10d9b5fbde918
Darien Hernandez <dh@hellofresh.com>
2017-07-02 10:43:24 +0200

    Added Event tests

---
+++ tests/HTTP/Event/AfterActionEventTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Test\Chassis\HTTP\Event;
+
+use Chassis\Infrastructure\HTTP\Event\AfterActionEvent;
+use PHPUnit\Framework\TestCase;
+use Tests\Chassis\MockHelpers;
+
+class AfterActionEventTest extends TestCase
+{
+    use MockHelpers;
+
+    public function testThatItCheckNameConstant()
+    {
+        $nameConstants = 'http.after.action';
+
+        $this->assertSame($nameConstants, AfterActionEvent::NAME);
+    }
+
+    public function testThatItCreateAnEvent()
+    {
+        $action = 'index';
+        $response = $this->mockResponse();
+
+        $event = new AfterActionEvent($action, $response);
+
+        $this->assertSame($action, $event->getAction());
+        $this->assertSame($response, $event->getResponse());
+    }
+}
---
+++ tests/HTTP/Event/BeforeActionEventTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace Test\Chassis\HTTP\Event;
+
+use Chassis\Infrastructure\HTTP\Event\BeforeActionEvent;
+use PHPUnit\Framework\TestCase;
+use Tests\Chassis\MockHelpers;
+
+class BeforeActionEventTest extends TestCase
+{
+    use MockHelpers;
+
+    public function testThatItCheckNameConstant()
+    {
+        $nameConstants = 'http.before.action';
+
+        $this->assertSame($nameConstants, BeforeActionEvent::NAME);
+    }
+
+    public function testThatItCreateAnEvent()
+    {
+        $action = 'index';
+        $request = $this->mockRequest();
+
+        $event = new BeforeActionEvent($action, $request);
+
+        $this->assertSame($action, $event->getAction());
+        $this->assertSame($request, $event->getRequest());
+    }
+}
